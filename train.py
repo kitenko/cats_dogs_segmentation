3ce953a01db9a128070fb2cb538a25d10e60ec6b
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-13 19:33:15 +0300

    minor

--- train.py
+++ train.py
@@ -4,13 +4,13 @@ import tensorflow as tf
 import segmentation_models as sm
 from tensorflow.keras.callbacks import EarlyStopping
 
-from models import ModelSegmentaions
+from models import build_model
 from data_generator import DataGenerator
 from config import (JSON_FILE_PATH, EPOCHS, SAVE_CURRENT_MODEL, TENSORBOARD_LOGS, FULL_NAME_MODEL, MODELS_DATA,
                     SAVE_MODELS, LOGS_DIR_CURRENT_MODEL, SAVE_CURRENT_TENSORBOARD_LOGS, LEARNING_RATE)
 
 
-def train(dataset_path_json: str) -> None:
+def train(dataset_path_json: str = JSON_FILE_PATH) -> None:
     """
     Training to classify generated images.
 
@@ -27,7 +27,7 @@ def train(dataset_path_json: str) -> None:
     train_data_gen = DataGenerator(json_path=dataset_path_json, is_train=True)
     test_data_gen = DataGenerator(json_path=dataset_path_json, is_train=False)
 
-    model = ModelSegmentaions().build_unet()
+    model = build_model()
     model.compile(optimizer=tf.keras.optimizers.Adam(LEARNING_RATE),
                   loss=sm.losses.categorical_focal_jaccard_loss,
                   metrics=['accuracy', sm.metrics.iou_score, sm.metrics.precision, sm.metrics.recall,
@@ -53,4 +53,4 @@ if __name__ == '__main__':
     devices = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(devices[0], True)
 
-    train(JSON_FILE_PATH)
+    train()
