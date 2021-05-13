c70f913dd54ff7632f40edc588ffb3473fa3c448
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-13 19:26:33 +0300

    minor

--- config.py
+++ config.py
@@ -8,20 +8,23 @@ NUMBER_CLASSES = 1 + 1
 INPUT_SHAPE_IMAGE = (256, 256, 3)
 PROPORTION_TEST_IMAGES = 0.2
 EPOCHS = 150
+LEARNING_RATE = 0.0001
+
+BACKBONE = 'resnet18'
+AUGMENTATION_DATA = True
+# ENCODER_WEIGHTS = None
+ENCODER_WEIGHTS = 'imagenet'
+ACTIVATION = 'softmax'
+NAME_MODEL = 'Unet'
+FULL_NAME_MODEL = NAME_MODEL + BACKBONE
 
 DATA_PATH = 'data'
 JSON_FILE_PATH = os.path.join(DATA_PATH, 'data.json')
 IMAGES_PATH = os.path.join(DATA_PATH, 'images')
 ANNOTATIONS_PATH = os.path.join(DATA_PATH, 'annotations')
 MASKS_PATH = os.path.join(ANNOTATIONS_PATH, 'masks')
-
-LEARNING_RATE = 0.0001
-BACKBONE = 'resnet18'
-# ENCODER_WEIGHTS = None
-ENCODER_WEIGHTS = 'imagenet'
-ACTIVATION = 'softmax'
-NAME_MODEL = 'Unet'
-FULL_NAME_MODEL = NAME_MODEL + BACKBONE
+LIST_FILE_PATH = os.path.join(ANNOTATIONS_PATH, 'list.txt')
+PATH_FOR_MODEL_WEIGHTS = 'models_data/save_models/Unet_imagenet_2021-05-13 16:31:31/Unetresnet18.h5'
 
 MODELS_DATA = 'models_data'
 TENSORBOARD_LOGS = os.path.join(MODELS_DATA, 'tensorboard_logs')
