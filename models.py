33b4b3f6d1526a3d0ad5d62cc462f1d27a31d825
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-13 19:30:06 +0300

    deleted Class

--- models.py
+++ models.py
@@ -3,36 +3,36 @@ from typing import Tuple
 import tensorflow as tf
 import segmentation_models as sm
 
-from config import BACKBONE, NUMBER_CLASSES, ACTIVATION, INPUT_SHAPE_IMAGE, ENCODER_WEIGHTS
+from config import BACKBONE, NUMBER_CLASSES, ACTIVATION, INPUT_SHAPE_IMAGE, ENCODER_WEIGHTS, NAME_MODEL
 
 
-class ModelSegmentaions():
-    def __init__(self, backbone_name: str = BACKBONE, num_classes: int = NUMBER_CLASSES, activation: str = ACTIVATION,
-                 image_shape: Tuple[int, int, int] = INPUT_SHAPE_IMAGE, encoder_weights: str = ENCODER_WEIGHTS) -> None:
-        """
-        This class creates a model from segmentation_models library depending on the input parameters.
-
-        :param backbone_name: name of classification model (without last dense layers) used as feature extractor to
-                              build segmentation model.
-        :param num_classes: number of classes
-        :param activation: name of one of keras.activations for last model layer (e.g. sigmoid, softmax, linear).
-        :param image_shape: this is image shape (height, width, channels).
-        :param encoder_weights: one of None (random initialization), imagenet (pre-training on ImageNet).
-        """
-        self.bakebone_name = backbone_name
-        self.num_classes = num_classes
-        self.activation = activation
-        self.image_shape = image_shape
-        self.encoder_weights = encoder_weights
-
-
-def build_unet():
+def build_model(backbone_name: str = BACKBONE, num_classes: int = NUMBER_CLASSES, activation: str = ACTIVATION,
+                image_shape: Tuple[int, int, int] = INPUT_SHAPE_IMAGE, encoder_weights: str = ENCODER_WEIGHTS,
+                name_model: str = NAME_MODEL) -> tf.keras.models.Model:
     """
-    This function builds Unet model based on the input parameters.
+    This function builds model based on the input parameters.
 
-    :return: tf.keras.model
+    :param backbone_name: name of classification model (without last dense layers) used as feature extractor to
+                          build segmentation model.
+    :param num_classes: number of classes
+    :param activation: name of one of keras.activations for last model layer (e.g. sigmoid, softmax, linear).
+    :param image_shape: this is image shape (height, width, channels).
+    :param encoder_weights: one of None (random initialization), imagenet (pre-training on ImageNet).
+    :param name_model: name model(Unet, Linknet, PSPNet, FPN).
+    :return: tf.keras.models.Model.
     """
-    model = sm.Unet(backbone_name=self.bakebone_name, encoder_weights=self.encoder_weights,
-                    classes=self.num_classes, activation=self.activation, input_shape=self.image_shape)
+    if name_model == 'Unet':
+        model = sm.Unet(backbone_name=backbone_name, encoder_weights=encoder_weights, classes=num_classes,
+                        activation=activation, input_shape=image_shape)
+    elif name_model == 'Linknet':
+        model = sm.Linknet(backbone_name=backbone_name, encoder_weights=encoder_weights, classes=num_classes,
+                           activation=activation, input_shape=image_shape)
+    elif name_model == 'PSPNet':
+        model = sm.PSPNet(backbone_name=backbone_name, encoder_weights=encoder_weights, classes=num_classes,
+                          activation=activation, input_shape=image_shape)
+    elif name_model == 'FPN':
+        model = sm.FPN(backbone_name=backbone_name, encoder_weights=encoder_weights, classes=num_classes,
+                       activation=activation, input_shape=image_shape)
+    else:
+        raise print('model name is wrong')
     return model
-
