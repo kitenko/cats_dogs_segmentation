a2cccc9d458f7ec65b2b937f3879ae85317bdf2c
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-15 22:01:41 +0300

    minor

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 
 from models import build_model
 from data_generator import DataGenerator
-from config import (JSON_FILE_PATH, EPOCHS, SAVE_CURRENT_MODEL, TENSORBOARD_LOGS, FULL_NAME_MODEL, MODELS_DATA,
+from config import (JSON_FILE_PATH, EPOCHS, SAVE_CURRENT_MODEL, TENSORBOARD_LOGS, MODELS_DATA, MODEL_NAME, BACKBONE,
                     SAVE_MODELS, LOGS_DIR_CURRENT_MODEL, SAVE_CURRENT_TENSORBOARD_LOGS, LEARNING_RATE)
 
 
@@ -17,12 +17,9 @@ def train(dataset_path_json: str = JSON_FILE_PATH) -> None:
     :param dataset_path_json: path to json file.
     """
     # create dirs
-    os.makedirs(TENSORBOARD_LOGS, exist_ok=True)
-    os.makedirs(MODELS_DATA, exist_ok=True)
-    os.makedirs(SAVE_MODELS, exist_ok=True)
-    os.makedirs(SAVE_CURRENT_MODEL, exist_ok=True)
-    os.makedirs(LOGS_DIR_CURRENT_MODEL, exist_ok=True)
-    os.makedirs(SAVE_CURRENT_TENSORBOARD_LOGS, exist_ok=True)
+    for p in [TENSORBOARD_LOGS, MODELS_DATA, SAVE_MODELS, SAVE_CURRENT_MODEL, LOGS_DIR_CURRENT_MODEL,
+              SAVE_CURRENT_TENSORBOARD_LOGS]:
+        os.makedirs(p, exist_ok=True)
 
     train_data_gen = DataGenerator(json_path=dataset_path_json, is_train=True)
     test_data_gen = DataGenerator(json_path=dataset_path_json, is_train=False)
@@ -35,7 +32,7 @@ def train(dataset_path_json: str = JSON_FILE_PATH) -> None:
                   )
     model.summary()
     early = EarlyStopping(monitor='loss', min_delta=0, patience=7, verbose=1, mode='auto')
-    checkpoint_filepath = os.path.join(SAVE_CURRENT_MODEL, FULL_NAME_MODEL+'.h5')
+    checkpoint_filepath = os.path.join(SAVE_CURRENT_MODEL, MODEL_NAME + BACKBONE + '.h5')
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         monitor='iou_score',
@@ -43,7 +40,8 @@ def train(dataset_path_json: str = JSON_FILE_PATH) -> None:
         save_best_only=True
     )
 
-    tensor_board = tf.keras.callbacks.TensorBoard(os.path.join(TENSORBOARD_LOGS, FULL_NAME_MODEL), update_freq='batch')
+    tensor_board = tf.keras.callbacks.TensorBoard(os.path.join(TENSORBOARD_LOGS, MODEL_NAME + BACKBONE),
+                                                  update_freq='batch')
     model.fit_generator(generator=train_data_gen, validation_data=test_data_gen, validation_freq=1,
                         validation_steps=len(test_data_gen), epochs=EPOCHS, workers=8,
                         callbacks=[early, model_checkpoint_callback, tensor_board])
